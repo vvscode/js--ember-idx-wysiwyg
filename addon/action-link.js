2e49231a2435ac4e6b19025b737318663a1bf998
vihryn <dzianis.tsishuk@upsilonit.com>
2016-05-04 14:10:39 +0300

    Refact action-link.

--- addon/action-link.js
+++ addon/action-link.js
@@ -8,36 +8,39 @@ export default Em.Component.extend(WithConfigMixin, {
   tagName: 'a',
   classNameBindings: ['styleClasses', 'activeClasses'],
   linkHref: '',
-  initModal: (function() {
+  initModal: (function () {
     var container = this.get('container');
-    if(!container.lookupFactory(COMPONENT_NAME)) {
+    if (!container.lookupFactory(COMPONENT_NAME)) {
       container._registry.register(COMPONENT_NAME, Modal.extend({
         layoutName: 'components/em-wysiwyg-action-link-modal',
         configName: 'bs',
         _parentView: this,
         linkHref: computed.alias('parentView.linkHref'),
         actions: {
-          addLink: function() {
+          addLink: function () {
             this.get('parentView').send('addLink');
-          }
-        }
+          },
+        },
       }));
     }
+
     this.set('modal', container.lookup(COMPONENT_NAME));
     return this.get('modal').append();
   }).on('init'),
-  styleClasses: (function() {
+  styleClasses: (function () {
     var _ref;
-    return (_ref = this.get('config.wysiwyg.actionClasses')) != null ? _ref.join(" ") : void 0;
+    return (_ref = this.get('config.wysiwyg.actionClasses')) != null ? _ref.join(' ') : void 0;
   }).property(),
-  activeClasses: (function() {
+  activeClasses: (function () {
     var _ref;
     if (this.get('active')) {
-      return (_ref = this.get('config.wysiwyg.actionActiveClasses')) != null ? _ref.join(" ") : void 0;
+      return (_ref = this.get('config.wysiwyg.actionActiveClasses')) != null ?
+        _ref.join(' ') :
+        void 0;
     }
   }).property('active'),
   actions: {
-    addLink: function() {
+    addLink: function () {
       this.get('editor').restoreSelection();
       this.get('editor').$().focus();
       if (this.get('linkHref')) {
@@ -45,25 +48,28 @@ export default Em.Component.extend(WithConfigMixin, {
       } else {
         document.execCommand('unlink', 0);
       }
+
       this.get('editor').saveSelection();
       this.get('wysiwyg').trigger('update_actions');
       return this.get('modal').close();
-    }
+    },
   },
-  click: function() {
+  click: function () {
     return this.get('modal').open();
   },
+
   wysiwyg: computed.alias('parentView.wysiwyg'),
   editor: computed.alias('wysiwyg.editor'),
-  listenToUpdate: (function() {
-    return this.get('wysiwyg').on('update_actions', (function(_this) {
-      return function() {
+  listenToUpdate: (function () {
+    return this.get('wysiwyg').on('update_actions', (function (_this) {
+      return function () {
         var container;
         container = _this.get('editor.selectionRange').commonAncestorContainer;
         if (container.nodeType === 3) {
           container = container.parentNode;
         }
-        if (container.nodeName === "A") {
+
+        if (container.nodeName === 'A') {
           _this.set('linkHref', Em.$(container).attr('href'));
           return _this.set('active', true);
         } else {
@@ -72,5 +78,5 @@ export default Em.Component.extend(WithConfigMixin, {
         }
       };
     })(this));
-  }).on('init')
+  }).on('init'),
 });
